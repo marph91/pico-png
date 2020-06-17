a0f1558f8136382ece0f7d881186e6a0ae89a8f0
Martin Dörfelt <martin.d@andix.de>
2020-06-17 17:25:14 +0200

    used the --no-formal flag
    This is done to suppress assertions, which leaded to an error during synthesis.

--- syn/synth.sh
+++ syn/synth.sh
@@ -20,7 +20,7 @@ ghdl -a --std=08 --work=png_lib "$ROOT/src/zlib.vhd"
 ghdl -a --std=08 --work=png_lib "$ROOT/src/row_filter.vhd"
 ghdl -a --std=08 --work=png_lib "$ROOT/src/png_encoder.vhd"
 # ghdl --synth --std=08 --work=png_lib png_encoder
-yosys -m ghdl -p 'ghdl --std=08 --work=png_lib png_encoder; synth_ice40 -json png_encoder.json'
-# nextpnr-ice40 --hx1k --package tq144 --json png_encoder.json --pcf constraints/png_encoder.pcf --asc png_encoder.asc
+yosys -m ghdl -p 'ghdl --std=08 --work=png_lib --no-formal png_encoder; synth_ice40 -json png_encoder.json'
+# nextpnr-ice40 --hx1k --package tq144 --json png_encoder.json --asc png_encoder.asc
 # icepack png_encoder.asc png_encoder.bin
 # iceprog png_encoder.bin

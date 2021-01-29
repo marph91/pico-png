4b4e9ced23f92ef9021568f65b74509429a9f3d8
Martin Dörfelt <martin.d@andix.de>
2021-01-29 19:03:29 +0100

    implemented a better model for lzss simulation

--- sim/lzss/run.py
+++ sim/lzss/run.py
@@ -1,30 +1,67 @@
 """Test cases for the lzss implementation."""
 
-from collections import namedtuple
+from dataclasses import dataclass
 from math import ceil, log2
 import os
 from os.path import join, dirname
+from typing import List, Union
 
 from vunit import VUnit
 
 
-def create_stimuli(root, name, input_data, output_data):
-    # TODO: implement full lzss in python
-    filename = join(root, "gen", "input_%s.csv" % name)
+def create_stimuli(root, case):
+    filename = join(root, "gen", f"input_{case.name}.csv")
     with open(filename, "w") as infile:
-        infile.write(", ".join(map(str, input_data)))
+        infile.write(", ".join(map(str, case.data_in)))
 
-    filename = join(root, "gen", "output_%s.csv" % name)
+    filename = join(root, "gen", f"output_{case.name}.csv")
     with open(filename, "w") as outfile:
-        outfile.write(", ".join(map(str, output_data)))
+        outfile.write(", ".join(map(str, case.data_out_int)))
+    return True
 
 
 def lb(size):
     return ceil(log2(size))
 
 
-def gen_literal(literal_value, input_buffer_size, search_buffer_size):
-    return literal_value << max(0, lb(input_buffer_size) + lb(search_buffer_size) - 8)
+@dataclass
+class Match:
+    offset: int
+    length: int
+
+
+@dataclass
+class Literal:
+    value: int
+
+
+@dataclass
+class Case:
+    name: str
+    input_buffer_size: int
+    search_buffer_size: int
+    data_in: List[int]
+    data_out: List[Union[Literal, Match]]
+
+    @property
+    def data_out_int(self) -> List[int]:
+        buffer_size = lb(self.input_buffer_size) + lb(self.search_buffer_size)
+        data_out_int = []
+        for datum in self.data_out:
+            if isinstance(datum, Literal):
+                # match structure:
+                # MSB: 1 (match), match offset, match length
+                value_int = datum.value << max(0, buffer_size - 8)
+            else:
+                # non match structure:
+                # MSB: 0 (non match), literal data, rest is ignored
+                value_int = (
+                    (1 << buffer_size) +
+                    (datum.offset << lb(self.search_buffer_size)) +
+                    datum.length
+                )
+            data_out_int.append(value_int)
+        return data_out_int
 
 
 def create_test_suite(tb_lib):
@@ -40,62 +77,36 @@ def create_test_suite(tb_lib):
                    "u": 7, "d": 8, "h": 9, "e": 10, "r": 11, ".": 12}
     complex_list = [encode_dict[letter] for letter in complex_sentence]
 
-    # structure of data_out in case of match:
-    # MSB: 1 (match), 12 bit: match distance, 4 bit: match length
-    # structure of data_out in case of non match:
-    # MSB: 0 (non match), 8 bit literal data, 8 bit ignored
-    Case = namedtuple("Case", ["name", "input_buffer_size",
-                               "search_buffer_size", "data_in", "data_out"])
     testcases = [
         # TODO: refactor: abstract literal and match
         Case("no_compression", 10, 12, [i for i in range(30)],
-             [gen_literal(i, 10, 12) for i in range(30)]),
+             [Literal(i) for i in range(30)]),
         # Case("rle", 5, 5, [0, 0, 0, 0, 1],
         #      [gen_literal(0, 5, 5),
         #       (1 << lb(5) + lb(5)) + (1 << lb(5)) + 3,
         #       gen_literal(1, 5, 5)
         #      ]),
         Case("rle_max_length", 20, 5, [0]*20 + [1],
-             [gen_literal(0, 20, 5),
-              (1 << lb(20) + lb(5)) + (1 << lb(5)) + 5,
-              (1 << lb(20) + lb(5)) + (5 << lb(5)) + 5,
-              (1 << lb(20) + lb(5)) + (5 << lb(5)) + 5,
-              (1 << lb(20) + lb(5)) + (5 << lb(5)) + 4,
-              gen_literal(1, 20, 5),
-             ]),
+             [Literal(0), Match(1, 5), Match(5, 5), Match(5, 5), Match(5, 4),
+              Literal(1)]),
         Case("repeat", 11, 10, [0, 1, 2, 0, 1, 2, 0, 1, 2, 0],
-             [gen_literal(0, 11, 10),
-              gen_literal(1, 11, 10),
-              gen_literal(2, 11, 10),
-              (1 << lb(11) + lb(10)) + (3 << lb(10)) + 7
-             ]),
+             [Literal(0), Literal(1), Literal(2), Match(3, 7)]),
         Case("complex", 10, 12, complex_list,
-             [gen_literal(0, 10, 12),
-              gen_literal(1, 10, 12),
-              gen_literal(2, 10, 12),
-              gen_literal(3, 10, 12),
-              gen_literal(4, 10, 12),
-              gen_literal(5, 10, 12),
-              gen_literal(6, 10, 12),
-              gen_literal(2, 10, 12),
-              gen_literal(7, 10, 12),
-              gen_literal(5, 10, 12),
-              (1 << lb(10) + lb(12)) + (8 << lb(12)) + 7,
-              gen_literal(encode_dict["n"], 10, 12),
-              gen_literal(encode_dict["d"], 10, 12),
-              (1 << lb(10) + lb(12)) + (12 << lb(12)) + 7,
-              gen_literal(encode_dict[" "], 10, 12),
-              gen_literal(encode_dict["h"], 10, 12),
-              gen_literal(encode_dict["e"], 10, 12),
-              gen_literal(encode_dict["r"], 10, 12),
-              gen_literal(encode_dict["u"], 10, 12),
-              gen_literal(encode_dict["m"], 10, 12),
-              gen_literal(encode_dict["."], 10, 12),
+             [Literal(0), Literal(1), Literal(2), Literal(3), Literal(4),
+              Literal(5), Literal(6), Literal(2), Literal(7), Literal(5),
+              Match(8, 7), Literal(encode_dict["n"]),
+              Literal(encode_dict["d"]),
+              Match(12, 7), Literal(encode_dict[" "]),
+              Literal(encode_dict["h"]),
+              Literal(encode_dict["e"]),
+              Literal(encode_dict["r"]),
+              Literal(encode_dict["u"]),
+              Literal(encode_dict["m"]),
+              Literal(encode_dict["."]),
              ]),
     ]
 
     for case in testcases:
-        print(case)
         generics = {
             "id": case.name,
             "C_INPUT_BUFFER_SIZE": case.input_buffer_size,
@@ -104,5 +115,4 @@ def create_test_suite(tb_lib):
         }
         tb_lzss.add_config(
             name=case.name, generics=generics,
-            pre_config=create_stimuli(root, case.name, case.data_in,
-                                      case.data_out))
+            pre_config=lambda: create_stimuli(root, case))
